71ae82ebcf9fd06434e953b6863e304d6155ff55
JALAL ABDESSAMAD <ABJALAL@royalairmaroc.com>
2023-07-04 16:51:40 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,487 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Chain App Dev - App Landing Page HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!--
+
+TemplateMo 570 Chain App Dev
+
+https://templatemo.com/tm-570-chain-app-dev
+
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header style="    background-color: white;  " class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a  style="width:100px" href="index.html" class="logo">
+              <img src="logo.png" alt="Chain App Dev">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+              <li class="scroll-to-section"><a href="#services">Services</a></li>
+              <li class="scroll-to-section"><a href="#about">About</a></li>
+              <li class="scroll-to-section"><a href="#pricing">Pricing</a></li>
+              
+            </ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+  
+  <div id="modal" class="popupContainer" style="display:none;">
+    <div class="popupHeader">
+        <span class="header_title">Login</span>
+        <span class="modal_close"><i class="fa fa-times"></i></span>
+    </div>
+
+    <section class="popupBody">
+        <!-- Social Login -->
+        <div class="social_login">
+            <div class="">
+                <a href="#" class="social_box fb">
+                    <span class="icon"><i class="fab fa-facebook"></i></span>
+                    <span class="icon_title">Connect with Facebook</span>
+
+                </a>
+
+                <a href="#" class="social_box google">
+                    <span class="icon"><i class="fab fa-google-plus"></i></span>
+                    <span class="icon_title">Connect with Google</span>
+                </a>
+            </div>
+
+            <div class="centeredText">
+                <span>Or use your Email address</span>
+            </div>
+
+            <div class="action_btns">
+                <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
+                <div class="one_half last"><a href="#" id="register_form" class="btn">Sign up</a></div>
+            </div>
+        </div>
+
+        <!-- Username & Password Login form -->
+        <div class="user_login">
+            <form>
+                <label>Email / Username</label>
+                <input type="text" />
+                <br />
+
+                <label>Password</label>
+                <input type="password" />
+                <br />
+
+                <div class="checkbox">
+                    <input id="remember" type="checkbox" />
+                    <label for="remember">Remember me on this computer</label>
+                </div>
+
+                <div class="action_btns">
+                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+                    <div class="one_half last"><a href="#" class="btn btn_red">Login</a></div>
+                </div>
+            </form>
+
+            <a href="#" class="forgot_password">Forgot password?</a>
+        </div>
+
+        <!-- Register Form -->
+        <div class="user_register">
+            <form>
+                <label>Full Name</label>
+                <input type="text" />
+                <br />
+
+                <label>Email Address</label>
+                <input type="email" />
+                <br />
+
+                <label>Password</label>
+                <input type="password" />
+                <br />
+
+                <div class="checkbox">
+                    <input id="send_updates" type="checkbox" />
+                    <label for="send_updates">Send me occasional email updates</label>
+                </div>
+
+                <div class="action_btns">
+                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+                    <div class="one_half last"><a href="#" class="btn btn_red">Register</a></div>
+                </div>
+            </form>
+        </div>
+    </section>
+</div>
+
+  <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-6 align-self-center">
+              <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <h1 style="color:aliceblue"> <strong>Best Quality IPTV Service</strong> </h1>
+                    <p style="color:aliceblue">#1 IPTV Provider Worldwide</p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="pricing" class="pricing-tables" style="padding-top: 0px;">
+    <div >
+      <div style="background-color: black"  class="row">
+        <div class="col-1 offset-2">
+          <img src="1.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="2.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="3.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="4.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="5.png" alt="">
+        </div>
+      
+        <div class="col-1">
+          <img src="7.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="8.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="9.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="10.png" alt="">
+        </div>
+      </div>
+      
+      <div style="background-color: black"  class="row">
+        <div style="background-color: black" class="col-1 offset-2">
+          <img src="andr.png" alt="">
+        </div>
+        <div style="background-color: black" class="col-1">
+          <img  src="i-phone-1.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="ipad.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="lg-smart-tv-logo-removebg-preview-1.png" alt="">
+        </div>
+        <div style="background-color: black"  class="col-1">
+          <img src="macos-x.png" alt="">
+        </div>
+        
+        <div class="col-1">
+          <img src="sma-1.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="tv-1.png" alt="">
+        </div>
+        <div class="col-1">
+          <img src="wind-1.png" alt="">
+        </div>
+       
+      </div>
+      <div class="row">
+        
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading">
+            <h4>Pick the   <em>Optimal</em> Plan for You :</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>Premium Features Included with Every Plan!</p>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <span class="price">$9.99</span>
+            <h4>1 MONTH</h4>
+            <div class="icon">
+              <img src="assets/images/pricing-table-01.png" alt="">
+            </div>
+            <ul>
+              <li>
+                <i class="fa-solid fa-tv"></i>Up to 15,000 Channels	</li>
+              <li><i class="fa-solid fa-tv"></i>Up to 40,000 Vods</li>
+              <li><i class="fa-solid fa-video"></i>8K / 4K Picture Quality</li>
+              <li >Without Freezing</li>
+              <li ><i class="fa-solid fa-cloud"></i>Server Uptime</li>
+              <li ><i class="fa-sharp fa-solid fa-headset"></i>24/7 Support</li>
+            </ul>
+            <div class="border-button">
+              <a href="#"><i class="fa-solid fa-cart-shopping"></i>Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-pro">
+            <span class="price">$24.99</span>
+            <h4>3 MONTHS</h4>
+            <div class="icon">
+              <img src="assets/images/pricing-table-01.png" alt="">
+            </div>
+            <ul>
+              <li>
+                <i class="fa-solid fa-tv"></i>Up to 15,000 Channels	</li>
+              <li><i class="fa-solid fa-tv"></i>Up to 40,000 Vods</li>
+              <li><i class="fa-solid fa-video"></i>8K / 4K Picture Quality</li>
+              <li >Without Freezing</li>
+              <li ><i class="fa-solid fa-cloud"></i>Server Uptime</li>
+              <li ><i class="fa-sharp fa-solid fa-headset"></i>24/7 Support</li>
+            </ul>
+            <div class="border-button">
+              <a href="#"><i class="fa-solid fa-cart-shopping"></i>Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <span class="price">$39.99</span>
+            <h4>6 MONTHS</h4>
+            <div class="icon">
+              <img src="assets/images/pricing-table-01.png" alt="">
+            </div>
+            <ul>
+              <li>
+                <i class="fa-solid fa-tv"></i>      Up to 15,000 Channels	</li>
+              <li><i class="fa-solid fa-tv"></i>Up to 40,000 Vods</li>
+              <li>8K / 4K Picture Quality</li>
+              <li >Without Freezing</li>
+              <li >Server Uptime</li>
+              <li >24/7 Support</li>
+            </ul>
+            <div class="border-button">
+              <a href="#">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <span class="price">$59.99</span>
+            <h4>12 MONTHS</h4>
+            <div class="icon">
+              <img src="assets/images/pricing-table-01.png" alt="">
+            </div>
+            <ul>
+              <li>
+                Up to 15,000 Channels	</li>
+              <li>Up to 40,000 Vods</li>
+              <li><i class="fa-solid fa-video"></i>8K / 4K Picture Quality</li>
+              <li >Without Freezing</li>
+              <li ><i class="fa-solid fa-cloud"></i>Server Uptime</li>
+              <li ><i class="fa-sharp fa-solid fa-headset"></i>24/7 Support</li>
+            </ul>
+            <div class="border-button">
+              <a href="#"><i class="fa-solid fa-cart-shopping"></i>Order Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+  
+
+  <div id="services" class="services section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
+            <h4>Multinational<em> channels </em> for you</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>IPTVELITE offers top-notch sports, movies, series, news, and adult films streaming services that stand out in the market due to their exceptional quality and unbeatable price.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="service-item first-service">
+            <div class="icon"></div>
+            <h4>8K/4K/ULTRA HD</h4>
+            <p>+15000(8K/4K/ULTRA HD) Tv Channels And VODs On Your Device</p>
+            
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="service-item second-service">
+            <div class="icon"></div>
+            <h4>ANTI FREEEZE PROTECTION</h4>
+            <p>Anti-freeze System Works For None Freeze On Any Device</p>
+            
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="service-item third-service">
+            <div class="icon"></div>
+            <h4>MONEY BACK GUATANTEE</h4>
+            <p>We offer 7 days Money Back Guarantee<a rel="nofollow" href="https://paypal.me/templatemo" target="_blank">a little via PayPal</a>. Thank you.</p>
+            
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+ 
+
+  <div id="about" class="about-us section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading">
+            <h4>Frequently <em>Asked</em> Questions </h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>Everything You Need to Know</p>
+          </div>
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">What Exactly is IPTV?</a></h4>
+                <p>it’s one of the most asked FAQ, IPTV which is also known as Internet Protocol Television is were internet is being used as a medium to send television channels to your TV at home. In other words, you use your internet connection to watch the TV Channels, movies and TV shows without an antenna or a dish.</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">What’s different with IPTV?</a></h4>
+                <p>Well, when you get a dish TV connection, it is important to install the dish connection and decoder. However, you don’t need to go through all that hassle with IPTV. You will be able to use the existing broadband connection to get media content. This is a cheaper and a hassle free alternative to traditional satellite TV.</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Requirements for IPTV?</a></h4>
+                <p>In fact, to use IPTV you need to be equipped with a high speed broadband connection. Speed of the broadband connection should be at least 4Mbps.</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">What are the payment options?</a></h4>
+                <p>You can pay for your service through Paypal. Payments will be taken via a secure gateway and there aren’t any hidden fees.</p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-image">
+            <img src="assets/images/about-right-dec.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  
+
+  <footer id="newsletter">
+    <div class="container">
+     
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="footer-widget">
+            <h4>Contact Us</h4>
+            <p>Rio de Janeiro - RJ, 22795-008, Brazil</p>
+            <p><a href="#">010-020-0340</a></p>
+            <p><a href="#">info@company.co</a></p>
+          </div>
+        </div>
+       
+      
+        <div class="col-lg-3">
+          <div class="footer-widget">
+            <h4>About Our Company</h4>
+            <div class="logo">
+              <img src="logo.png" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="copyright-text">
+            <p>Copyright © 2023 Chain App Dev Company. All Rights Reserved. 
+          <br>Design: <a href="https://templatemo.com/" target="_blank" title="css templates">TemplateMo</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+</body>
+</html>
